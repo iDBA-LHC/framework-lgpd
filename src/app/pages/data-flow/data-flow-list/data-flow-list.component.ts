8f8dbc9d7719d4c5667c6dc694f44e38a5468bb4
Rodrigo <rodrigo.gts@hotmail.com>
2021-08-29 18:38:20 -0300

    :sparkles: Finalizado listagem de registros DataFlow e filtro de listagem

--- src/app/pages/data-flow/data-flow-list/data-flow-list.component.ts
+++ src/app/pages/data-flow/data-flow-list/data-flow-list.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
-//import { AreaService } from 'src/app/services/area.service';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { DataFlowService } from 'src/app/services/data-flow.service';
 import { CustomSnackBarService } from 'src/app/shared/components/custom-snack-bar/custom-snack-bar.service';
-import { ExportPdfService } from 'src/app/services/export-pdf.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { Area } from 'src/app/models/area/area';
+import { DataFlow } from 'src/app/models/data-flow/data-flow';
 import { TrataExcessaoConexao } from 'src/app/shared/utils/trata-excessao-conexao';
 
 @Component({
@@ -14,62 +13,55 @@ import { TrataExcessaoConexao } from 'src/app/shared/utils/trata-excessao-conexa
 })
 export class DataFlowListComponent implements OnInit {
 
-  //isLoading = false;
-  //
-  //displayedColumns: string[] = ["nomeArea", "nomeEmpresa", "nomeResponsavel", "actions"];
-  //
-  //dataSource = new MatTableDataSource();
-  //@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  //@ViewChild(MatSort, { static: false }) sort: MatSort;
+  isLoading = false;
+  displayedColumns: string[] = ["nomeProcessamento", "actions"];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
 
   constructor(
-    //private areaService: AreaService,
-    //private snackBar: CustomSnackBarService,
-    //private dialog: MatDialog,
-    //private exportPdfService: ExportPdfService,
+    private dataFlowService: DataFlowService,
+    private snackBar: CustomSnackBarService,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
-    //this.pesquisaAreas();
-    console.log('Carregou DataFlow!!! Wiiii!!')
+    this.pesquisaDataFlows();
   }
-  /*
-    pesquisaAreas() {
-      this.isLoading = true;
-      this.areaService.listaTodasAreas().subscribe(
-        (response) => {
-          this.isLoading = false;
-          this.dataSource = new MatTableDataSource<Area>(response.body);
-          setTimeout(() => {
-            this.dataSource.filterPredicate = (
-              data: { nomeArea: string,
-                      nomeEmpresa: number },
-              filterValue: string
-            ) => data.nomeArea.toString().trim().toLowerCase().indexOf(filterValue) !== -1 ||
-                 data.nomeEmpresa.toString().trim().toLowerCase().indexOf(filterValue) !== -1 ;
-      
-            this.dataSource.paginator = this.paginator;
-            this.dataSource.sort = this.sort;          
-          });
-        },
-        (err) =>{
-          if (err.status === 401)
-          {
-            TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => {this.pesquisaAreas();}));
-          }
-          else
-          {
-            this.isLoading = false;
-            TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
-          }
+
+  pesquisaDataFlows() {
+    this.isLoading = true;
+    this.dataFlowService.listaTodosDataFlows().subscribe((response) => {
+      this.isLoading = false;
+      console.log(response.body)
+      this.dataSource = new MatTableDataSource<DataFlow>(response.body);
+      setTimeout(() => {
+        this.dataSource.filterPredicate = (
+          data: {
+            nomeProcessamento: string,
+          },
+          filterValue: string
+        ) => data.nomeProcessamento.toString().trim().toLowerCase().indexOf(filterValue) !== -1
+
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
+    },
+      (err) => {
+        if (err.status === 401) {
+          TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => { this.pesquisaDataFlows(); }));
         }
-      );    
-    }
-  
-    applyFilter(value: string) {
-      this.dataSource.filter = value.trim().toLowerCase();
-    } */
+        else {
+          this.isLoading = false;
+          TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
+        }
+      }
+    );
+  }
+
+  applyFilter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
 
 }
