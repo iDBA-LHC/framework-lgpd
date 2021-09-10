6f6e0fb1b46aa195f61e6f5bbd903e2c22442159
Daniel Flores Fontana <daniel@floresfontana.com.br>
2021-09-10 13:27:50 -0300

    estrutura inicial data flow

--- src/app/models/data-flow/data-flow.ts
+++ src/app/models/data-flow/data-flow.ts
@@ -1,11 +1,18 @@
+import { Compartilhamento } from "../compartilhamento/compartilhamento";
+import { LocalArmazenamento } from "../local-armazenamento/local-armazenamento";
+import { Usuario } from '../usuario/usuario';
+
 export class DataFlow {
   codDataFlow: number;
+  nomeProcessamento: string;
   codCicloMonitoramento: number;
   codAtividade: number;
   codMetadados: number;
-  nomeProcessamento: string;
   indDescarte: number;
   indRisco: number;
   codCicloVida: number;
   codUsuarioInclusao: number;
+  usuarios: Array<Usuario>;
+  armazenamentos: Array<LocalArmazenamento>;
+  compartilhamentos: Array<Compartilhamento>;
 }
--- src/app/pages/data-flow/data-flow-form/data-flow-form.component.ts
+++ src/app/pages/data-flow/data-flow-form/data-flow-form.component.ts
@@ -1,13 +1,19 @@
+import { Usuario2 } from './../../../models/usuario/usuario2';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Compartilhamento } from 'src/app/models/compartilhamento/compartilhamento';
 import { DataFlow } from 'src/app/models/data-flow/data-flow';
+import { LocalArmazenamento } from 'src/app/models/local-armazenamento/local-armazenamento';
 import { AuthService } from 'src/app/services/auth.service';
 import { CompartilhamentoService } from 'src/app/services/compartilhamento.service';
 import { DataFlowService } from 'src/app/services/data-flow.service';
+import { LocalArmazenamentoService } from 'src/app/services/local-armazenamento.service';
 import { CustomSnackBarService } from 'src/app/shared/components/custom-snack-bar/custom-snack-bar.service';
 import { TrataExcessaoConexao } from 'src/app/shared/utils/trata-excessao-conexao';
+import { Usuario } from 'src/app/models/usuario/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-data-flow-form',
@@ -17,8 +23,17 @@ import { TrataExcessaoConexao } from 'src/app/shared/utils/trata-excessao-conexa
 export class DataFlowFormComponent implements OnInit {
 
   dataFlowForm: FormGroup;
-  dataFlowId: number;
+  codDataFlow: number;
   isLoading = false;
+
+  listaArmazenamentos: LocalArmazenamento[];
+  listaArmazenamentosFiltrados: LocalArmazenamento[];
+
+  listaCompartilhamentos: Compartilhamento[];
+  listaCompartilhamentosFiltrados: Compartilhamento[];
+
+  listaUsuarios: Usuario[];
+  listaUsuariosFiltrados: Usuario[];
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -28,7 +43,9 @@ export class DataFlowFormComponent implements OnInit {
     private router: Router,
     private dialog: MatDialog,
     private DataFlowService: DataFlowService,
-    private compartilhamentoService: CompartilhamentoService
+    private usuarioService: UsuarioService,
+    private compartilhamentoService: CompartilhamentoService,
+    private localArmazenamentoService: LocalArmazenamentoService,
   ) { }
 
   ngOnInit() {
@@ -39,58 +56,108 @@ export class DataFlowFormComponent implements OnInit {
   }
 
   private createForm() {
-    this.dataFlowForm = this.formBuilder.group({      
-	  //codCicloMonitoramento: ["", Validators.required],
+    this.dataFlowForm = this.formBuilder.group({
+      nomeProcessamento: ["", Validators.required],
+      codCicloMonitoramento: ["", Validators.required],
+      codAtividade: ["", Validators.required],
+      codMetadados: ["", Validators.required],
+      indDescarte: ["", Validators.required],
+      indRisco: ["", Validators.required],
+      codCicloVida: ["", Validators.required],
+      codUsuarioInclusao: ["", Validators.required],
+      armazenamentos: ["", Validators.required],
+      compartilhamentos: ["", Validators.required],
+      usuarios: ["", Validators.required],
+      //codCicloMonitoramento: ["", Validators.required],
     });
   }
 
   pesquisaDataFlow() {
     this.activatedRoute.params.subscribe(
       (data) => {
-        this.dataFlowId = parseInt(data["id?"]);
-        
-        if (this.dataFlowId) {
-          this.DataFlowService.pesquisaDataFlow(this.dataFlowId).subscribe(
+        this.codDataFlow = parseInt(data["id?"]);
+
+        if (this.codDataFlow) {
+          this.DataFlowService.pesquisaDataFlow(this.codDataFlow).subscribe(
             (retorno) => {
               this.dataFlowForm.patchValue({
-                codDataFlow: retorno.body[0].codDataFlow                
+                codDataFlow: retorno.body[0].codDataFlow,
+
+                nomeProcessamento: retorno.body[0].nomeProcessamento,
+
+                codCicloMonitoramento: retorno.body[0].codCicloMonitoramento,
+
+                codAtividade: retorno.body[0].codAtividade,
+
+                codMetadados: retorno.body[0].codMetadados,
+
+                indDescarte: retorno.body[0].indDescarte,
+
+                indRisco: retorno.body[0].indRisco,
+
+                codCicloVida: retorno.body[0].codCicloVida,
+
+                codUsuarioInclusao: retorno.body[0].codUsuarioInclusao,
+
+                armazenamentos: retorno.body[0].armazenamentos,
+
+                compartilhamentos: retorno.body[0].compartilhamentos,
               });
+
+              this.preencherCombos();
             },
             (err) => {
-              if (err.status === 401)
-                {
-                  TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => {this.pesquisaDataFlow();}));
-                }
-                else
-                {
-                  TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
-                }
+              if (err.status === 401) {
+                TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => { this.pesquisaDataFlow(); }));
+              }
+              else {
+                TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
+              }
             }
           );
+        } else {
+          this.preencherCombos();
         }
       }
     )
   }
 
+  preencherCombos() {
+
+    this.pesquisaLocalArmazenamento();
+
+    this.pesquisaCompartilhamentos();
+
+  }
+
   salvarDataFlow() {
     if (this.dataFlowForm.valid) {
       const DataFlow: DataFlow = this.dataFlowForm.getRawValue();
-      DataFlow.codDataFlow = this.dataFlowId;      
+      DataFlow.codDataFlow = this.codDataFlow;
 
-      if (this.dataFlowId) {
+      DataFlow.indRisco = (this.dataFlowForm.controls.indRisco.value ? 1 : 0);
+      DataFlow.indDescarte = (this.dataFlowForm.controls.indDescarte.value ? 1 : 0);
+
+      var usuarios2 = new Array();
+      DataFlow.usuarios.forEach(function (e, i) {
+        let usuario2: Usuario2 = new Usuario2;
+        usuario2.codUsuario = e.codigoUsuario;
+        usuarios2.push(usuario2);
+      });
+      DataFlow.usuarios = usuarios2;
+
+      if (this.codDataFlow) {
         // Alteração
         this.DataFlowService.alterarDataFlow(DataFlow).subscribe(
           (response) => {
-            this.snackBar.openSnackBar(`O Data Map foi atualizado com sucesso!`,null);
-              this.router.navigate(["/data-flow"]);
+            this.snackBar.openSnackBar(`O Data Map foi atualizado com sucesso!`, null);
+            this.router.navigate(["/data-flow"]);
           },
           (err) => {
-            if (err.status === 401)
-            {
-              TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => {this.salvarDataFlow();}));
+            if (err.status === 401) {
+              TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => { this.salvarDataFlow(); }));
             }
-            else
-            {
+            else {
               TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
             }
           }
@@ -99,21 +166,81 @@ export class DataFlowFormComponent implements OnInit {
         // Inclusão
         this.DataFlowService.incluirDataFlow(DataFlow).subscribe(
           (response) => {
-            this.snackBar.openSnackBar(`O Data Map foi criado com sucesso!`,null);
+            this.snackBar.openSnackBar(`O Data Map foi criado com sucesso!`, null);
             this.router.navigate(["/data-flow"]);
           },
           (err) => {
-            if (err.status === 401)
-            {
-              TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => {this.salvarDataFlow();}));
+            if (err.status === 401) {
+              TrataExcessaoConexao.TrataErroAutenticacao(err, this.snackBar, this.authService.renewSession(() => { this.salvarDataFlow(); }));
             }
-            else
-            {
+            else {
               TrataExcessaoConexao.TrataExcessao(err, this.snackBar);
             }
           }
         )
       }
     }
-  }  
+  }
+
+  private pesquisaLocalArmazenamento() {
+    this.localArmazenamentoService.listaTodosLocaisArmazenamento().subscribe(
+      (retorno) => {
+        this.listaArmazenamentosFiltrados = retorno.body;
+
+        //let codigoEmpresa = this.dataMapForm.controls.codigoEmpresa.value;
+        //this.listaArmazenamentosFiltrados = <LocalArmazenamento []>this.listaArmazenamentos.filter(model => model.codigoEmpresa == codigoEmpresa);
+      }
+    )
+    this.isLoading = false;
+  }
+
+  compareArmazenamento(o1: any, o2: any): boolean {
+    if (o2 != null)
+      return o1.codLocalArmazenamento === o2.codLocalArmazenamento;
+  }
+
+  private pesquisaCompartilhamentos() {
+    this.compartilhamentoService.listarTodosCompartilhamentos().subscribe(
+      (retorno) => {
+        this.listaCompartilhamentosFiltrados = retorno.body;
+
+        //let codigoEmpresa = this.dataMapForm.controls.codigoEmpresa.value;
+        //this.listaCompartilhamentosFiltrados = <Compartilhamento []>this.listaCompartilhamentos.filter(model => model.codigoEmpresa == codigoEmpresa);
+      }
+    )
+    this.isLoading = false;
+  }
+
+  compareCompartilhamento(o1: any, o2: any): boolean {
+    if (o2 != null)
+      return o1.codCompartilhamento === o2.codCompartilhamento;
+  }
+
+  compareUsuarioSelecionado(o1: any, o2: any): boolean {
+    if (o2 != null)
+      return o1.codigoUsuario === o2.codUsuario;
+  }
+
+  private pesquisaUsuarios() {
+    this.usuarioService.listaTodosUsuarios().subscribe(
+      (retorno) => {
+        this.listaUsuarios = retorno.body;
+
+        let codEmpresa = this.dataFlowForm.controls.codEmpresa.value;
+
+        this.listaUsuariosFiltrados = <Usuario[]>this.listaUsuarios.filter(usuario => usuario.codigoEmpresa == codEmpresa);
+      }
+    )
+    this.isLoading = false;
+  }
+
+  selecionaUsuario(event) {
+    let usuarioSelecionado: Usuario = event.option.value;
+    this.dataFlowForm.controls.usuarios.setValue(usuarioSelecionado);
+  }
+
+  displayUsuario(usuario: Usuario): string {
+    return usuario ? usuario.nomeUsuario : "";
+  }
+
 }
