89ff51ec4a4221eef0601811ae4bb59e62bcee68
Marcelo Crestani Brazil <marcelo.brazil@idba.com.br>
2022-05-17 08:31:28 -0300

    Ajustes para criação de ambientes do priva em novo servidor

---
+++ src/environments/environment.homologacao-interno.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  envName: "homologação",
+  apiURL: "http://10.1.0.9:3001/api/v1/",
+  envDesc: "Homologação",
+  timeout: 6000000,
+  encryptionKey: "_iDB@Fram3w0rk_M@r1oneT3",
+  tipoUsuaruioAdmin: 5,
+  tipoUsuarioEncarregado: 4,
+  tipoUsuarioOperador: 3
+};
