4366cd30b05eac8b3e5ecdbee235bec4f9b4abab
Marcelo Crestani Brazil <marcelo.brazil@idba.com.br>
2022-05-18 08:21:40 -0300

    Ajustes nas configuraçõs dos novos ambientes

--- src/environments/environment.bellube.ts
+++ src/environments/environment.bellube.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: "",
-  apiURL: "https://privahmlmysql.idba.com.br/api/v1/",
+  apiURL: "https://bellube.privacenter.com.br/api/v1/",
   envDesc: "",
   timeout: 6000000,
   encryptionKey: "_iDB@Fram3w0rk_M@r1oneT3",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   envName: "dev",
   apiURL: "http://localhost:8080/api/v1/",
-  apiURLAD: "http://10.0.1.50:3010/api-idba/authenticate-ad",
   envDesc: "Desenvolvimento",
   timeout: 6000000,
   encryptionKey: "_iDB@Fram3w0rk_M@r1oneT3",
