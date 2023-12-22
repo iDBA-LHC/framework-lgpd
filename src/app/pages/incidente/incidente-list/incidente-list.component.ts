dbf4f4a26453106e3a73a13e81c34f95146263df
Marcelo Crestani Brazil <marcelo.brazil@idba.com.br>
2023-12-22 13:17:40 -0300

    Criação do relatório de incidente

--- src/app/pages/incidente/incidente-list/incidente-list.component.ts
+++ src/app/pages/incidente/incidente-list/incidente-list.component.ts
@@ -213,18 +213,115 @@ export class IncidenteListComponent implements OnInit {
 
     }
 
-    exportAllToPDF() {
-        const pages = document.querySelector('.all-pages') as HTMLElement;
+    gerarPdfIncidente(incidente: Incidente) {
+        console.log(incidente);
+        var indIncidenteNoPrazo = false;
+        var linha = 70; 
+        var saltoLinha = 15;
+
         const doc = new jsPDF({
             unit: 'px',
             format: [595, 842]
         });
 
-        doc.html(pages, {
-            callback: (doc: jsPDF) => {
-                doc.save('pdf-export');
-            }
-        });
+        let empresaIncidente: Empresa = <Empresa>this.listaEmpresas.filter(empresa => empresa.codigoEmpresa == incidente.codigoEmpresa)[0];
+
+        this.imprimeCabecalhoRelatorio(doc);
+
+        doc.setFontSize(16);
+        doc.setTextColor(0,0,0);
+        doc.text(`Incidente # ${incidente.numeroProtocolo}`, 250, 50);
+
+        doc.text(`Controladora: ${incidente.nomeEmpresa}`, 10, linha);
+        doc.text(`CNPJ: ${this.cnpjCpfPipe.transform(incidente.numeroCNPJEmpresa)} `, 300, linha);
+        linha+=saltoLinha;
+        doc.text(`Operador: ${incidente.nomeUsuarioOperador}`, 10, linha);
+        linha+=saltoLinha;
+        doc.text(`Encarregado: ${incidente.nomeUsuarioEncarregado}`, 10, linha);
+        linha+=saltoLinha;
+        doc.text(`E-Mail Encarregado: ${incidente.emailUsuarioEncarregado}`, 10, linha);
+        linha+=saltoLinha;
+        doc.text(`Telefone Controladora: ${empresaIncidente ? empresaIncidente.telefoneControlador : ""}`, 10, linha);
+        linha+=saltoLinha;
+        doc.text(`Data/Hora de Registro: ${this.datePipe.transform(incidente.dataRegistro,'dd/MM/yyyy hh:mm',"UTC")} `, 10, linha);
+        doc.text(`Data/Hora do Incidente: ${this.datePipe.transform(incidente.dataIncidente,'dd/MM/yyyy hh:mm',"UTC")} `, 300, linha);
+        linha+=saltoLinha;
+        doc.text(`Data de Comunicação Registro: ${this.datePipe.transform(incidente.dataComunicacao,'dd/MM/yyyy',"UTC")} `, 10, linha);
+
+        if (this.datediff(incidente.dataIncidente, incidente.dataComunicacao) > 2) {
+			indIncidenteNoPrazo = false;
+		}
+		else {
+			indIncidenteNoPrazo = true;
+		}
+
+        if (indIncidenteNoPrazo)
+        {
+            doc.setTextColor(0,255,0);
+            doc.text(`Incidente Comunicado No Prazo`, 300, linha);
+        }
+        else
+        {
+            doc.setTextColor(255,0,0);
+            doc.text(`Incidente Comunicado Fora do Prazo`, 300, linha);
+        }
+
+        linha+=saltoLinha;
+        doc.setTextColor(0,0,0)
+        
+        doc.text(`Justificativa:`,10, linha);
+        doc.text(`${incidente.desJustificativa ? incidente.desJustificativa : ""}`, 80,linha);
+        linha+=(saltoLinha * this.contaLinhas(incidente.desJustificativa));
+        doc.text(`Status: ${this.statusIncidenteButtons.buttonsForm[incidente.indStatus - 1].description}`, 10,linha);
+        linha+=saltoLinha;
+        doc.text(`Tipo de Comunicação: ${incidente.desTipoComunicacao ? incidente.desTipoComunicacao: ''}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Dados do Agente de Tratamento: ${incidente.dadosAgenteTratamento ? incidente.dadosAgenteTratamento: ""}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Dados do Notificante: ${incidente.dadosNotificante ? incidente.dadosNotificante: ""}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Detalhes:`,10, linha);
+        doc.text(`${incidente.desDetalhes ? incidente.desDetalhes: ""}`,65, linha);
+        linha+=(saltoLinha * this.contaLinhas(incidente.desDetalhes));
+        doc.text(`Natureza dos Dados Afetados: ${incidente.desNaturezaDados ? incidente.desNaturezaDados: ""}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Tipo Titulares Afetados: ${incidente.desTipoTitulares ? incidente.desTipoTitulares: ""}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Medidas Preventivas:`,10,linha);
+        doc.text(`${incidente.desMedidasPreventivas ? incidente.desMedidasPreventivas: ""}`,128, linha);
+        linha+=(saltoLinha * this.contaLinhas(incidente.desMedidasPreventivas));
+        doc.text(`Medidas Mitigatórias:`,10,linha);
+        doc.text(`${incidente.desMedidasMitigatorias ? incidente.desMedidasMitigatorias: ""}`,128, linha);
+        linha+=(saltoLinha * this.contaLinhas(incidente.desMedidasMitigatorias));
+        doc.text(`Relatório de Impacto: ${incidente.indRelatorioImpacto ? 'Sim':'Não'}`,10, linha);
+        linha+=saltoLinha;
+        doc.text(`Consequências:`,10,linha);
+        doc.text(`${incidente.desConsequencias ? incidente.desConsequencias: ""}`,100, linha);
+        linha+=(saltoLinha * this.contaLinhas(incidente.desConsequencias));
+        doc.text(`Link Documento Incidente: ${incidente.desLinkDocumento ? incidente.desLinkDocumento : ''}`,10, linha);
+
+        doc.save('pdf-export');
+        
     }
+
+    private imprimeCabecalhoRelatorio(doc: jsPDF)
+    {
+        doc.setFillColor("F58634");
+        doc.rect(0, 0, 595, 30, 'F');
+        doc.setFontSize(20);
+        doc.setTextColor(255, 255, 255);
+        doc.text('PRIVA',280,21);
+    }
+
+    private contaLinhas(texto: string): number
+    {
+        var linhas = texto.split("\n").length;
+        if (linhas === 0) linhas = 1;
+        return linhas;
+    }
+
+    private datediff(first, second) {
+		return Math.round((second - first) / (1000 * 60 * 60 * 24)) + 1;
+	}
 
 }
