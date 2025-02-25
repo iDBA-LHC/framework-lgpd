03aca95af2e3c8cb48924596eb262a81a8400e2d
Marcelo Crestani Brazil <marcelo.brazil@idba.com.br>
2025-02-25 09:49:59 -0300

    Criação de novo ambiente para nova empresa Nexus Ligas

---
+++ src/environments/environment.nexus-ligas.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  envName: "",
+  apiURL: "https://nexusliga.privacenter.com.br/api/v1/",
+  //apiURL: "http://10.1.0.9:3006/api/v1/",
+  envDesc: "",
+  timeout: 6000000,
+  encryptionKey: "_iDB@Fram3w0rk_M@r1oneT3",
+  tipoUsuaruioAdmin: 5,
+  tipoUsuarioEncarregado: 4,
+  tipoUsuarioOperador: 3
+};
